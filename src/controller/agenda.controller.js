ad736256af8e0a8c37aaea23da3429c5a4228fb1
Tia <tia21100@gmail.com>
2021-10-22 01:34:53 +0700

    lil

--- src/controller/agenda.controller.js
+++ src/controller/agenda.controller.js
@@ -4,14 +4,14 @@ const { agendaService } = require('../services');
 const pick = require('../utils/pick');
 
 const createAgenda = catchAsync(async (req, res) => {
-  const agenda = await agendaService.createAgenda(req.body, req.user.id, req.subproject.id);
+  const agenda = await agendaService.createAgenda(req.body, req.user.id);
   res.status(httpStatus.CREATED).send(agenda);
 });
 
 const getAgendas = catchAsync(async (req, res) => {
   const filter = { user: req.user.id };
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
-  const result = await agendaService.querySubprojects(filter, options);
+  const result = await agendaService.queryAgenda(filter, options);
   res.send(result);
 });
 
